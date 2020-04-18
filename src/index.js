00f62b0167756723e1358770bcc1a8a51256884d
Matt Huston <hustonapps@gmail.com>
2020-04-18 11:58:17 -0600

    login page completed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { loadTheme } from 'office-ui-fabric-react';
 import './index.css';
 import App from './pages/App';
 import Login from './pages/Login';
 import * as serviceWorker from './serviceWorker';
+
+loadTheme({
+  defaultFontStyle: { fontFamily: 'Roboto, sans-serif' },
+  palette: {
+    themePrimary: '#AB61E5',
+    themeLighterAlt: '#fbf8fe',
+    themeLighter: '#f1e4fb',
+    themeLight: '#e5ccf7',
+    themeTertiary: '#cc9cf0',
+    themeSecondary: '#b572e9',
+    themeDarkAlt: '#9b57cf',
+    themeDark: '#8349ae',
+    themeDarker: '#603681',
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,6 +28,12 @@ ReactDOM.render(
       <Route path="/app" component={App} />
       <Route exact path="/" component={Login} />
     </Router>
+    <div className="footer">
+      <img src="../../assets/Theorem.png" alt="Theorem" />
+      <span>
+        Copyright &copy; {new Date().getFullYear()} <span style={{ fontWeight: 700 }}>Theorem</span> All Rights Reserved
+      </span>
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import './Login.css';
+import { PrimaryButton } from 'office-ui-fabric-react';
+import AppIcon from '../../assets/AppIcon.png';
 
-const LoginPage = () => (
-  <div>
-    <h1>Login</h1>
-  </div>
-);
+const LoginPage = ({ history }) => {
+  const handleLogin = () => {
+    history.push('/app');
+  };
+
+  return (
+    <div className="LoginPage">
+      <div className="LoginContainer">
+        <div className="logo">
+          <img src={AppIcon} alt="Honesto" />
+          <h3>Honesto</h3>
+        </div>
+        <PrimaryButton text="Login with Google" onClick={handleLogin} />
+      </div>
+    </div>
+  );
+}
+
+LoginPage.propTypes = {
+  history: PropTypes.object.isRequired,
+};
 
 export default LoginPage;
