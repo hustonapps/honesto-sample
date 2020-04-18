c5779b06fb758fc840455d9a758f1f3a731e2a34
Matt Huston <hustonapps@gmail.com>
2020-04-18 12:11:00 -0600

    prod build

---
+++ build/precache-manifest.bc9d81ed79b848c1a488b8481f5aab93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d57985eb9d9303b436f55b7b64bd533",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0be5a03b35b503296ae5",
+    "url": "/static/css/main.fd1c754a.chunk.css"
+  },
+  {
+    "revision": "b9e21647745b4ef144a6",
+    "url": "/static/js/2.12b9dc40.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.12b9dc40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0be5a03b35b503296ae5",
+    "url": "/static/js/main.03f41abe.chunk.js"
+  },
+  {
+    "revision": "0fda49cb828de9744b2b",
+    "url": "/static/js/runtime-main.d264639c.js"
+  },
+  {
+    "revision": "51430e96f5d6af9f2668371ccfa3ea0c",
+    "url": "/static/media/Background.51430e96.png"
+  }
+]);
