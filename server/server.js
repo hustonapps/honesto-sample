ec16a7acfb3a99ba436aae690988d8740166402a
Matt Huston <hustonapps@gmail.com>
2020-04-18 11:30:06 -0600

    static routing in place

--- server/server.js
+++ server/server.js
@@ -1,8 +1,17 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const app = express();
 const port = 4040;
 const { connectDb, seedData } = require('./db');
+
+app.use('/static', express.static(path.join(__dirname, '../build/static')));
+app.get('/app', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
 
 connectDb().then(async () => {
   console.log('db connected');
